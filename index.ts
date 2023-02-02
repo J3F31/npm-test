2f88010438d4b86276b9c104975a6f504c3f523d
Jero Eyras <jero.eyras@gmail.com>
2023-02-02 12:12:15 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
-import { MeshBuilder } from 'babylonjs'
+import { Color3, MeshBuilder, StandardMaterial } from 'babylonjs'
+
+const types = ['sphere', 'cube']
 
 export function helloWorld() {
 	const message = 'Hello World from my example modern npm package!';
@@ -10,19 +12,21 @@ export function goodBye() {
 	return message;
 }
 
-export function TestMesh(name: string, diameter: number, scene: any) {
-	const ball = MeshBuilder.CreateSphere(name, {diameter: diameter}, scene);
-	return ball;
-}
-
-export function Some() {
-	const message = 'aa';
-	return message;
+export function TestMesh(name: string, type: string, size: number, scene: any, materialColor: Color3) {
+	if (!types.includes(type)) {
+		console.error(`type must be: ${types}`)
+		return
+	}
+	const mesh = 
+		type == 'sphere'? MeshBuilder.CreateSphere(name, {diameter: size}, scene) : 
+		type == 'cube'? MeshBuilder.CreateBox(name, {size: size}, scene) : null;
+	const mat = new StandardMaterial("generatedMat", scene);
+	mat.diffuseColor = materialColor;
+	return mesh;
 }
   
 export default {
 	helloWorld,
 	goodBye,
-	TestMesh,
-	Some
+	TestMesh
 };
